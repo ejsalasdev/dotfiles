c70a42cf08deeb567d76cdd18a295de353a3e182
Edgar J. Salas <ejsalasdev@gmail.com>
2025-11-26 21:18:57 -0500

    fix(git): convert nvim submodule to regular folder

---
+++ config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,39 @@
+return {
+  -- 1. Instalar Catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+      },
+    },
+  },
+
+  -- 2. Configurar LazyVim para usar este tema
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
