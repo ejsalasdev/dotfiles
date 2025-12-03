fc90341482cd924b1c6d7fb1493bd1f1984e5794
Edgar J. Salas <ejsalasdev@gmail.com>
2025-12-02 20:44:11 -0500

    feat(hypr): add clipboard history with cliphist
    Implemented clipboard history functionality using cliphist.
    - Added cliphist to install.sh.
    - Added autostart commands for cliphist to hyprland.conf.
    - Bound Mod+V to open clipboard history menu via Wofi.
    - Remapped togglefloating to Mod+Shift+Space for consistency.

--- install.sh
+++ install.sh
@@ -85,7 +85,7 @@ install_packages() {
         UTILS_PKGS=(
             "grim" "slurp" "wl-clipboard" "git" "curl" "brightnessctl"
             "starship" "bash-completion" "fzf" "eza" "bat" "bc" "make" "man-db" "sane-airscan"
-	    "ntfs-3g"
+	    "ntfs-3g" "cliphist"
         )
 
         APPS_PKGS=(
