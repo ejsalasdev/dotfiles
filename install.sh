4138d1bec80b868c535d777ab96a2936c21b05d1
Edgar J. Salas <ejsalasdev@gmail.com>
2025-12-06 22:33:09 -0500

    revert(system): switch back to wofi from hyprlauncher
    Reverted the migration to hyprlauncher due to styling/maturity issues.
    - Restored 'wofi' keybinding (Super+Space) in hyprland.conf.
    - Removed hyprlauncher from install.sh and deleted config files.
    - Removed hyprlauncher window rules.

--- install.sh
+++ install.sh
@@ -95,7 +95,7 @@ install_packages() {
             "viewnior"
             "cups" "cups-pdf" "cups-filters" "gutenprint" "simple-scan"
             "libreoffice-still" "libreoffice-still-es" "bluetui"
-            "hyprsunset" "hyprlauncher"
+            "hyprsunset"
         )
 
         ALL_PACKAGES=("${{CORE_PKGS[@]}}" "${{FILE_MANAGER_PKGS[@]}}" "${{AUDIO_PKGS[@]}}" "${{THEME_PKGS[@]}}" "${{UTILS_PKGS[@]}}" "${{APPS_PKGS[@]}}")
